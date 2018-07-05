b1dc0cea02b087d30d49e98fb56a9d6d93bf624c
Vaibhav Nandam <vaibhavnandam@vaibhavs-air.home>
2018-07-05 19:10:39 -0400

    MainStory board done and added to controller

--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -9,17 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var progressBar: UIView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func answerPressed(_ sender: UIButton) {
+        
     }
-
+    
 
 }
 
